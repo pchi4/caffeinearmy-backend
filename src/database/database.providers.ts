e62b34450df472901fd93db8115e8532e2b0e0b6
Douglas Lopes <pchialoopes@gmail.com>
2022-08-24 15:54:09 -0300

    Inicializando projeto

---
+++ src/database/database.providers.ts
@@ -0,0 +1,23 @@
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'mysql',
+        host: process.env.MYSQL_HOST,
+        port: Number.parseInt(process.env.MYSQL_PORT),
+        username: 'root',
+        password: 'root',
+        database: process.env.MYSQL_DB_NAME,
+        entities: [
+            __dirname + '/../**/*.entity{.ts,.js}',
+        ],
+        synchronize: true,
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
