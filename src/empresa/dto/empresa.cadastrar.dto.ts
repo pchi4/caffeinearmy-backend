d62a34a9622e9b1fd380ef75e63a7d836f66ff2c
Douglas Lopes <pchialoopes@gmail.com>
2022-08-27 13:33:52 -0300

    Alterando o type de number para string no cnpj

--- src/empresa/dto/empresa.cadastrar.dto.ts
+++ src/empresa/dto/empresa.cadastrar.dto.ts
@@ -1,13 +1,13 @@
 export interface EmpresaCadastrarDto {
-    cnpj:number;
+    cnpj:string;
     nomeFantasia: string;
     email:string;
     telefone:string;
     razaoSocial: string;
-    lojista:{
+/*     lojista:{
         nome:string;
         email:string;
         telefone1:string;
         telefone2:string;
-    }
+    } */
 }
