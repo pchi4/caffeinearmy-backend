65f4f98ccaec08d0883c27f57f9fd0cd6f3aa42b
Douglas Lopes <pchialoopes@gmail.com>
2022-08-27 17:56:41 -0300

    Importando a EmpresaModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmpresaModule } from './empresa/empresa.module';
 import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [UsuarioModule, EmpresaModule],
   controllers: [AppController],
   providers: [AppService],
 })
