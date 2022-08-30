870d41b16e5e3fca2ff075dc41a63b8882444625
Douglas Lopes <pchialoopes@gmail.com>
2022-08-30 00:37:06 -0300

    Fix conflits

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -9,7 +8,6 @@ async function bootstrap() {
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true})),
   await app.listen(3001);
 }
 bootstrap();
