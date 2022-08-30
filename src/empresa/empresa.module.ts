c75e9abec8af74cf83cd89adbed48053c5232bf2
Douglas Lopes <pchialoopes@gmail.com>
2022-08-30 00:16:50 -0300

    Update model mepresa

--- src/empresa/empresa.module.ts
+++ src/empresa/empresa.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from 'src/database/database.module';
+import { DatabaseModule } from '../database/database.module';
 import { EmpresaController } from './empresa.controller';
 import { EmmpresaService } from './empresa.services';
 import { empresaProviders } from './empresa.providers';
 
 @Module({
-    imports:[DatabaseModule],
-    controllers:[EmpresaController],
-    providers: [
-        ...empresaProviders,
-        EmmpresaService,
-    ],
+  imports: [DatabaseModule],
+  controllers: [EmpresaController],
+  providers: [...empresaProviders, EmmpresaService],
 })
-
-export class EmpresaModule{}
+export class EmpresaModule {}
--- src/empresa/empresa.services.ts
+++ src/empresa/empresa.services.ts
@@ -1,49 +1,49 @@
-import { Inject, Injectable } from "@nestjs/common"
-import { Repository } from "typeorm";
-import { Empresa } from "./empresa.entity";
-import { EmpresaCadastrarDto } from "./dto/empresa.cadastrar.dto";
-import { ResultadoDto } from "src/dto/resultado.dto";
+import { Inject, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Empresa } from './empresa.entity';
+import { EmpresaCadastrarDto } from './dto/empresa.cadastrar.dto';
+import { ResultadoDto } from 'src/dto/resultado.dto';
 
 @Injectable()
-export class EmmpresaService{
-    constructor(
-        @Inject('EMPRESA_REPOSITORY')
-        private empresaRepository: Repository<Empresa>,
-    ) {}
+export class EmmpresaService {
+  constructor(
+    @Inject('EMPRESA_REPOSITORY')
+    private empresaRepository: Repository<Empresa>,
+  ) {}
 
-    async findAll(): Promise<Empresa[]>{
-        return this.empresaRepository.find();
-    }
+  async findAll(): Promise<Empresa[]> {
+    return this.empresaRepository.find();
+  }
 
-    async getEmpresa(cnpj: string): Promise<Empresa> {
-      return this.empresaRepository.findOne({where:{cnpj:cnpj}});
-    }
-    async cadastrar(data: EmpresaCadastrarDto): Promise<ResultadoDto>{
-        let empresa = new Empresa()
-                
-        empresa.cnpj = data.cnpj
-        empresa.nomeFantasia = data.nomeFantasia
-        empresa.email = data.email
-        empresa.telefone = data.telefone
-        empresa.razaoSocial = data.razaoSocial
-        empresa.nomeLojista = data.nomeLojista
-        empresa.emailLojista = data.emailLojista
-        empresa.telefoneLojista1 = data.telefoneLojista1
-        empresa.telefoneLojista2 = data.telefoneLojista2
-        
-        return this.empresaRepository.save(empresa)
-          .then((result)=>{
-            return <ResultadoDto>{
-              status: true,
-              mensage: "Usuario cadastrado com sucesso",
-            }
-          })
-          .catch((error)=>{
-            return <ResultadoDto>{
-              status: false,
-              mensage: "Houve um erro ao cadastar",
-            }
-          })   
-      }
-    
+  async getEmpresa(cnpj: string): Promise<Empresa> {
+    return this.empresaRepository.findOne({ where: { cnpj: cnpj } });
+  }
+  async cadastrar(data: EmpresaCadastrarDto): Promise<ResultadoDto> {
+    const empresa = new Empresa();
+
+    empresa.cnpj = data.cnpj;
+    empresa.nomeFantasia = data.nomeFantasia;
+    empresa.email = data.email;
+    empresa.telefone = data.telefone;
+    empresa.razaoSocial = data.razaoSocial;
+    empresa.nomeLojista = data.nomeLojista;
+    empresa.emailLojista = data.emailLojista;
+    empresa.telefoneLojista1 = data.telefoneLojista1;
+    empresa.telefoneLojista2 = data.telefoneLojista2;
+
+    return this.empresaRepository
+      .save(empresa)
+      .then((result) => {
+        return <ResultadoDto>{
+          status: true,
+          mensage: 'Usuario cadastrado com sucesso',
+        };
+      })
+      .catch((error) => {
+        return <ResultadoDto>{
+          status: false,
+          mensage: 'Houve um erro ao cadastar',
+        };
+      });
+  }
 }
