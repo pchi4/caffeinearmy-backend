d18e30ba4a8ede633b10f86ccb1997b8e2fc2435
Douglas Santos <douglas.santos@ssp.ba.gov.br>
2023-02-10 13:16:45 -0300

    novas features

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,25 +1,35 @@
-import { Body, Controller, Get, Post, UseGuards, Request, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  Request,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from '../auth/auth.service';
 import { ResultadoDto } from 'src/dto/resultado.dto';
 import { UsuarioCadastrarDto } from './dto/usuario.cadastro.dto';
 import { Usuario } from './usuario.entity';
 import { UsuarioService } from './usuario.service';
+import { AuthDto } from 'src/auth/dto/auth.dto';
 
 @Controller('usuario')
 export class UsuarioController {
   constructor(
     private readonly usuarioService: UsuarioService,
     private authService: AuthService,
-    ) {}
+  ) {}
 
-    @Get('listar')
-    async findAll(): Promise<Usuario[]> {
-      return this.usuarioService.findAll();
-    }
+  @Get('listar')
+  async findAll(): Promise<Usuario[]> {
+    return this.usuarioService.findAll();
+  }
 
   @Get(':id')
-  async findOneById(@Param('id',  ParseIntPipe) id: number, ): Promise<Usuario>{
+  async findOneById(@Param('id', ParseIntPipe) id: number): Promise<Usuario> {
     return this.usuarioService.findOneById(id);
   }
 
@@ -28,9 +38,8 @@ export class UsuarioController {
     return this.usuarioService.cadastrar(body);
   }
 
-  @UseGuards(AuthGuard('local'))
   @Post('login')
-  async login(@Request() req) {
-    return this.authService.gerarToken(req.user);
+  async login(@Body() data: AuthDto) {
+    return this.authService.validateUser(data.email, data.password);
   }
 }
